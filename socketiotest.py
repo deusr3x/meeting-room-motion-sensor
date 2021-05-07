029fee61dc7f19e37bf9a4662ada545ca0c649c2
Alex <ahoare@gmail.com>
2021-05-07 17:44:16 +1000

    adjustments

--- socketiotest.py
+++ socketiotest.py
@@ -5,13 +5,13 @@ import datetime
 sio = socketio.Client()
 sio.connect("http://0.0.0.0:8000") # change to server address
 print('my sid is', sio.sid)
-message = {'room': 'c_188c9bc7tbb1shu3ngtt936nq9dos4g8e9nmmt1ecdnmq@resource.calendar.google.com', 'status': 'open'}
+message = {'room': '', 'status': 'open'}
 #sio.emit('meeting_room_update', message)
 
 try:
     while True:
         a = datetime.datetime.now()
-        message = {'room': 'c_188c9bc7tbb1shu3ngtt936nq9dos4g8e9nmmt1ecdnmq@resource.calendar.google.com', 'status': f'{a}'}
+        message = {'room': '', 'status': f'{a}'}
         sio.emit('meeting_room_update', message)
         time.sleep(10)
 except KeyboardInterrupt:
